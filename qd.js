0b0a52e5c26abf4d0b1842f29196923ea9fbc49f
Clarity-89 <homes89@ukr.net>
2015-04-10 00:42:04 +0300

    upd

--- qd.js
+++ qd.js
@@ -1,46 +1,79 @@
 /**
  * Created by Alex on 5/4/15.
  */
-window.onload = function () {
+(function () {
+
+    function setContent(ids, text) {
+        if (ids.length > 1 && Array.isArray(ids)) {
+            ids.map(function (el, i) {
+                el.innerHTML = '<strong>' + text[i] + '</strong>';
+            });
+        } else {
+            ids.innerHTML = '<strong>' + text + '</strong>';
+        }
+    }
+
+    function setClass(ids, name) {
+        if (ids.length > 1 && Array.isArray(ids)) {
+            ids.map(function (el) {
+                el.className = name;
+            });
+        } else {
+            ids.className = name;
+        }
+    }
+
     function solveQuadratic() {
         var a = parseInt(document.getElementById("a").value, 10),
             b = parseInt(document.getElementById("b").value, 10),
             c = parseInt(document.getElementById("c").value, 10),
             solution = document.getElementById("solution"),
             solution2 = document.getElementById("solution2"),
+            dang = "alert alert-danger",
+            success = "alert alert-success",
             x, dis = (b * b) - (4 * a * c), x1, x2;
-        solution.innerHTML = null;
-        solution2.innerHTML = null;
-        solution2.className = '';
-        if ((b * b) < 4 * a * c) {
-            solution.className = "alert alert-danger";
-            solution.innerHTML = "<strong>The solution is not a real number!</strong>"
+
+        setContent([solution, solution2], ['', '']);
+        setClass(solution2, '');
+        if (isNaN(a) || isNaN(b) || isNaN(c)) {
+            setContent(solution, "Make sure all the values are numbers!");
+            setClass(solution, dang);
+        } else if ((b * b) < 4 * a * c && b !== 0) {
+            setClass(solution, dang);
+            setContent(solution, "The solution is not a real number!");
         } else if (a === 0 && b === 0) {
-            solution.className = "alert alert-danger";
-            solution.innerHTML = "<strong>Both a and b cannot be 0!</strong>";
+            setClass(solution, dang);
+            setContent(solution, "Both a and b cannot be 0!");
         }
         else if (a === 0 && b !== 0) {
             x = -c / b;
-            solution.className = "alert alert-success";
-            solution.innerHTML = "<strong>x = " + x + "</strong>";
-            console.log(x);
+            setClass(solution, success);
+            setContent(solution, "x = " + x);
         }
         else if (a !== 0 && b === 0) {
             x = Math.sqrt(-c / a);
-            solution.className = "alert alert-success";
-            solution.innerHTML = "<strong>x = " + x + "</strong>";
+            setClass(solution, success);
+            setContent(solution, "x = " + x);
         }
         else if (a === 0 && c === 0) {
-            solution.className = "alert alert-info";
-            solution.innerHTML = "<strong>x = 0</strong>";
+            setClass(solution, success);
+            setContent(solution, "x = 0");
         } else {
             x1 = (-b + Math.sqrt(dis)) / (2 * a);
             x2 = (-b - Math.sqrt(dis)) / (2 * a);
-            solution.className = "alert alert-success";
-            solution.innerHTML = "<strong> x1 = " + x1 + "</strong>";
-            solution2.className = "alert alert-success";
-            solution2.innerHTML = "<strong> x2 = " + x2 + "</strong>";
+            setClass([solution, solution2], success);
+            setContent([solution, solution2], ["x1 = " + x1, "x2 = " + x2]);
         }
     }
+
+    document.getElementById("forms").onkeydown = function (e) {
+        if (e.which == 13 || e.keyCode == 13) {
+            document.getElementById("calc").click();
+            return false;
+        }
+        return true;
+    };
+
     document.getElementById("calc").onclick = solveQuadratic;
-};
+
+})();
